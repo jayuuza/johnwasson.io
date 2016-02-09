1b5fc86c2d2315db4c2707ec3dcce9efc5dd3c18
John <wassonjonathan@gmail.com>
2016-02-09 20:03:45 +0200

    .ideas ignored. Added more to notable projects. Linked gravatar
    My picture is now pulled directly from the gravatar api. Finished the
    rest of my basic info. Finished notable projects for now

--- home/views.py
+++ home/views.py
@@ -1,12 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import urllib, hashlib
 
 
 # Create your views here.
 def index(request):
+    email = "wassonjonathan@gmail.com"
+    default = ""
+    size = 300
+
+    # construct the url
+    gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
+    gravatar_url += urllib.urlencode({'d':default, 's':str(size)})
     my_variables = {
         "page_home": "active",
-        "page_title": "Jonathan Wasson | Software Developer"
+        "page_title": "Jonathan Wasson | Software Developer",
+        "gravatar_url": gravatar_url
     }
 
     return render(request, 'home/index.html', my_variables)
